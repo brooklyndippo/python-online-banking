d059b4bb9f4b71248c72103f4433dc2fefb065d0
Brooklyn Dippo <brooklyndippo@gmail.com>
2021-10-05 07:10:18 -1000

    created files, wrote pseudocode

---
+++ script.py
@@ -0,0 +1,11 @@
+'''
+Write a function to load Write a function to load data. Parameter: file name, returns a list of users.
+Write a function to display the user information. Parameters: one or more strings, or an array and return a formatted string.
+Write a login function that takes username and password as parameters and returns a user dictionary.
+'''
+#initialize an empty dictionary with banking customers
+banking_customers = {}
+
+
+def load_data(filename):
+    return banking_customers
