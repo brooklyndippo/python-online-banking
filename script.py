5cf22967e09eae2ac5f6f110292ea7df9408db7c
Brooklyn Dippo <brooklyndippo@gmail.com>
2021-10-05 08:59:10 -1000

    added additional verifications, comments. Complete!

--- script.py
+++ script.py
@@ -1,70 +1,58 @@
 '''
-Write a function to load Write a function to load data. Parameter: file name, returns a list of users.
-Write a function to display the user information. Parameters: one or more strings, or an array and return a formatted string.
-Write a login function that takes username and password as parameters and returns a user dictionary.
-
-
 INITIALIZE empty dictionary banking_customers
+IMPORT text files
 '''
 
 banking_customers = {
     #'username': ("password", "customer_name", "account_balance")
 }
 customer_data_file = "data.txt"
+bank_file = "pythonbank.txt"
 
 '''
-FUNCTION load_data(filename):
-    IMPORT data.txt file
-    SPLIT at each comma to create a list
-    SET username = key 
-    APPEND to dictionary banking_customers
-    SET password, name, balance = pairs
-    APPEND to dictionary banking_customers
-
-    RETURN banking_customers
+Fucntion LOAD ART loads the ascii art file.
+It removes the line break at the end of each line and prints each line to show the art.
 '''
 
+def load_art(filename):
+    file = open(filename, "r")
+    lines = file.readlines()
+    for line in lines:
+        art_line = line.replace("\n", "")
+        print (art_line)
+
+'''
+Function LOAD DATA loads the user data and returns a dictionary with account info.
+>> Note: uncomment print values to confirm if data import & sort is working
+'''
 def load_data(filename):
     file = open(filename, "r")
-    print (file)
     lines = file.readlines()
-    print (lines)
     for line in lines:
-        print ("========================")
-        print(line)
+        #print ("========================")
+        #print(line)
         value = line.split(',')
-        print (value)
-        print ("========================")
+        #print (value)
+        #print ("========================")
         username = value[0]
-        print (f"username: {username}")
+        #print (f"username: {username}")
         password = value[1]
-        print (f"password: {password}")
+        #print (f"password: {password}")
         name = value[2]
-        print (f"name: {name}")
-        balance = value[3]
-        int(balance)
-        print (f"balance: {balance}")
+        #print (f"name: {name}")
+        balance = int(value[3]) #convert the balance from a string to an integer
+        #print (f"balance: {balance}")
 
         banking_customers[username]=password, name, balance
-        print (banking_customers)
-        print ("========================")
+        #print (banking_customers)
+        #print ("========================")
 
     file.close()
     return banking_customers
 
-
-load_data(customer_data_file)
-
 '''
-FUNCTION display_account_info(username):
-    SET customer_name = GET dictionary element with key
-    SET account_balance = GET dictionary element with key
-
-    DISPLAY ("customer_name: Your account balance is $account_balance")
-    RETURN ("customer_name, account_balance")
-
+Function DISPLAY ACCOUNT INFO displays the customer's account information, including the current balance.
 '''
-
 def display_account_info(username):
     customer_account = banking_customers[username]
     
@@ -76,41 +64,50 @@ def display_account_info(username):
     print (f"Your account balance is:")
     print ("")
     print (f"${account_balance}")
-
-display_account_info("betho")
-'''
-
-
-FUNCTION login(username, password):
-    SET correct_password = GET dictionary element with key(username)
-
-    IF the password == correct_password:
-        THEN display_account_info(username)
-    
-    ELSE:
-        DISPLAY "Your login information was incorrect. Please try again."
+    print ("")
 
 '''
-
+Function LOGIN takes in a customers username and password to validate the account.
+    *If the login information is accurate, it displays account information.
+    *If it's not accurate, it reprompts for a username & password. 
+'''
 def login():
 
     user_input = input("Username:")
     user_password = input("Password:")
 
-    account_match = banking_customers[user_input]
-    correct_password = account_match[0]
-    print (correct_password)
+    #check to see if the username is valid
+    if user_input in banking_customers:
+        account_match = banking_customers[user_input]
+        correct_password = account_match[0]
+        #print (correct_password) - only do this to test the program
 
-    if user_password == correct_password:
-        display_account_info(user_input)
+        #check to see if the password matches
+        if user_password == correct_password:
+            display_account_info(user_input)
 
     else:
+        print ("")
         print("Username and password not found.")
+        print("Please try again.")
         print("")
         login()
 
 
+'''
+Run the online banking app! 
+'''
+
+#Load the customer data first
+load_data(customer_data_file)
+
+#Display the bank & welcome message
+print ("")
+load_art(bank_file)
+print ("")
 print ("Welcome to Bank of Python")
 print ("To get started, please login below:")
 print ("")
+
+#Run the login function
 login()
