71b5853aca4c8ed1d247e97226923c6d9aa60f62
Brooklyn Dippo <brooklyndippo@gmail.com>
2021-10-05 07:53:13 -1000

    successfully imported data, appended data to dictionary

--- script.py
+++ script.py
@@ -2,10 +2,80 @@
 Write a function to load Write a function to load data. Parameter: file name, returns a list of users.
 Write a function to display the user information. Parameters: one or more strings, or an array and return a formatted string.
 Write a login function that takes username and password as parameters and returns a user dictionary.
-'''
-#initialize an empty dictionary with banking customers
-banking_customers = {}
 
+
+INITIALIZE empty dictionary banking_customers
+'''
+
+banking_customers = {
+    #'username': ("password", "customer_name", "account_balance")
+}
+customer_data_file = "data.txt"
+
+'''
+FUNCTION load_data(filename):
+    IMPORT data.txt file
+    SPLIT at each comma to create a list
+    SET username = key 
+    APPEND to dictionary banking_customers
+    SET password, name, balance = pairs
+    APPEND to dictionary banking_customers
+
+    RETURN banking_customers
+'''
 
 def load_data(filename):
+    file = open(filename, "r")
+    print (file)
+    lines = file.readlines()
+    print (lines)
+    for line in lines:
+        print ("========================")
+        print(line)
+        value = line.split(',')
+        print (value)
+        print ("========================")
+        username = value[0]
+        print (f"username: {username}")
+        password = value[1]
+        print (f"password: {password}")
+        name = value[2]
+        print (f"name: {name}")
+        balance = value[3]
+        print (f"balance: {balance}")
+
+        banking_customers[username]=password, name, balance
+        print (banking_customers)
+        print ("========================")
+
+    file.close()
     return banking_customers
+
+
+load_data(customer_data_file)
+
+'''
+FUNCTION display_account_info(username):
+    SET customer_name = GET dictionary element with key
+    SET account_balance = GET dictionary element with key
+
+    DISPLAY ("customer_name: Your account balance is $account_balance")
+    RETURN ("customer_name, account_balance")
+
+FUNCTION login(username, password):
+    SET correct_password = GET dictionary element with key(username)
+
+    IF the password == correct_password:
+        THEN display_account_info(username)
+    
+    ELSE:
+        DISPLAY "Your login information was incorrect. Please try again."
+
+
+NOTE dictionary format:
+
+banking_customers = {
+    'username': [password, customer_name, account_balance],
+    'username': [password, customer_name, account_balance]
+}
+'''
